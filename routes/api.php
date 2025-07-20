69ecd23b6dbe13f671a116d20b182e2b2bf333d4
Hashem ElGendy <hashemelgendy02@gmail.com>
2025-07-20 16:29:49 +0300

    ref: notes apis

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Http\Controllers\NoteController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthController;
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', fn($request) => $request->user());
-    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('notes', NoteController::class)->only(['index', 'store', 'update', 'destroy']);
 });
 
 Route::post('/register', [AuthController::class, 'register']);
