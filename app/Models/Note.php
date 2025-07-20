fabbd4d0bc248b70553e91a593216cf742aa0da1
Hashem ElGendy <hashemelgendy02@gmail.com>
2025-07-20 19:57:14 +0300

    ref: add user relation to note

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Note extends Model
 {
@@ -10,4 +11,9 @@ class Note extends Model
         'title',
         'body',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
